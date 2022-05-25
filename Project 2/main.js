68c6a412a497be8477fb788c3ed9dd4961670c2f
AshAdamsCodes <97040516+AshAdamsCodes@users.noreply.github.com>
2022-05-24 21:56:05 -0400

    Add files via upload
    initial upload of project 2

---
+++ Project 2/main.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    }
+
+
+
+    $(document).ready(() => {
+$('.hide-button').on('click', () => {
+$('.first-image').hide();
+})
+    });
+
+    
+
+
+
